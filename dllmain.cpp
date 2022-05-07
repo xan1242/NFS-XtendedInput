235c88da22f599bbb0ee488f3d1b06448e4e2f32
xan1242 <8014093+xan1242@users.noreply.github.com>
2022-05-07 17:14:52 +0200

    update todo

--- dllmain.cpp
+++ dllmain.cpp
@@ -8,6 +8,7 @@
 // TODO: kill DInput enough so that it doesn't detect XInput controllers but still detects wheels
 // TODO: proper raw input for keyboard (and maybe non XInput gamepads?)
 // TODO: implement the Controls settings menu - it should be possible to make it talk to the INI
+// TODO: loading screen control tips
 // TODO (MW): Max performance button is visible in all submenus during Customize for some reason...
 // TODO (MW): CUSTOMIZE MENU IS BUGGY - during career cash status overlaps the max performance buttons...
 // TODO (Carbon): fix mouse wheel zooming (in FE only) and keyboard zooming during photo mode
