94a5b74cf172e42dadab251a549ed68a232b07de
xan1242 <8014093+xan1242@users.noreply.github.com>
2022-05-09 14:00:34 +0200

    update todo

--- dllmain.cpp
+++ dllmain.cpp
@@ -10,7 +10,7 @@
 // TODO: implement the Controls settings menu - it should be possible to make it talk to the INI
 // TODO: loading screen control tips
 // TODO: mouselook sensitivity
-// TODO: mouselook jerky movement
+// TODO: mouselook jerky movement -- update the mouse centering somewhere else perhaps
 // TODO (MW): Max performance button is visible in all submenus during Customize for some reason...
 // TODO (MW): CUSTOMIZE MENU IS BUGGY - during career cash status overlaps the max performance buttons...
 // TODO (Carbon): fix mouse wheel zooming (in FE only) and keyboard zooming during photo mode
