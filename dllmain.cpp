578d23c08310f78e0c38a84f3de8014b518e03e6
xan1242 <8014093+xan1242@users.noreply.github.com>
2023-01-29 23:31:16 +0100

    [MW] Windows 7 build config test

--- dllmain.cpp
+++ dllmain.cpp
@@ -27,12 +27,12 @@
 #include "NFS_XtendedInput.h"
 #include "NFS_XtentedInput_ActionID.h"
 
-#if (_WIN32_WINNT >= 0x0602 /*_WIN32_WINNT_WIN8*/)
-#include <XInput.h>
-#pragma comment(lib,"xinput.lib")
-#else
+#ifdef XINPUT_OLD
 #include <XInput.h>
 #pragma comment(lib,"xinput9_1_0.lib")
+#else
+#include <XInput.h>
+#pragma comment(lib,"xinput.lib")
 #endif
 
 #include "NFS_XtendedInput_XInputConfig.h"
